6bee7474e73855cbec5cf02770f7d1c089239c99
mironalin <alinmiron3@gmail.com>
2024-05-20 07:59:32 -0700

    Add logos, install shadcn dependencies and next-themes for implement changeable themes

---
+++ app/(page)/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-orange-600 text-3xl">Hello Index Page!</p>
+    </div>
+  );
+}
