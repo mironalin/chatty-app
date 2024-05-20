3d57ab0c490719ef43abd99dd876eb9cad09ff6c
mironalin <alinmiron3@gmail.com>
2024-05-20 09:09:18 -0700

    Modify the light theme bg color and start preparing for the Auth UI

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-orange-600 text-3xl">Hello Index Page!</p>
+    <div className="flex min-h-full flex-col justify-center py-12 sm: px-6 lg:px-8">
+      {/* <ThemeToggle /> */}
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image height={96} width={96} className="mx-auto w-[96px]" src="/images/svg/logo-no-background.svg" alt="Chatty logo"></Image>
+        <h2 className="mt-6 text-3xl text-center font-medium tracking-tight text-gray-800">Sign in to your account</h2>
+      </div>
     </div>
   );
 }
