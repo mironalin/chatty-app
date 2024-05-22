1f41832be17889f629bb8e51030e0d3f7c0079cf
mironalin <alinmiron3@gmail.com>
2024-05-21 19:16:20 -0700

    Rework the AvatarStatus component

--- components/ui/activitystatus.tsx
+++ components/ui/activitystatus.tsx
@@ -1,41 +1,8 @@
 const ActivityStatus = () => {
   return (
     <div>
-      {" "}
-      <span
-        className="
-      hidden
-      absolute
-      rounded-full
-      bg-green-500
-      ring-2
-      ring-background
-      top-0
-      right-0
-      h-2
-      w-2
-      md:h-3
-      md:w-3
-      dark:block
-    "
-      />
-      <span
-        className="
-
-    absolute
-    rounded-full
-    bg-green-500
-    ring-2
-    ring-background
-    top-0
-    right-0
-    h-2
-    w-2
-    md:h-3
-    md:w-3
-    dark:hidden
-    "
-      />
+      <span className="hidden absolute rounded-full bg-green-500 ring-2 ring-background top-0 right-0 h-2 w-2 md:h-3 md:w-3 dark:block" />
+      <span className="absolute rounded-full bg-green-500 ring-2 ring-background top-0 right-0 h-2 w-2 md:h-3 md:w-3 dark:hidden" />
     </div>
   );
 };
